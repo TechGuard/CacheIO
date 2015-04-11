6ffae10d88ce8dd00fc37d6b8b28b703936e4b85
Robin van Nunen <robin@vnunen.nl>
2015-04-11 14:40:24 +0200

    Default project

---
+++ src/CacheIO/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CacheIO
+{
+    public class Class1
+    {
+		public static string getText()
+		{
+			return "Hello world!";
+		}
+    }
+}
---
+++ src/Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CacheIO;
+
+namespace Example
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Class1.getText());
+		}
+	}
+}
